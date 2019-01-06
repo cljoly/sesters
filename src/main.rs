4c369d94f8e0e359c3ce227a77297db8d3980a33
Leo <leowzukw@171819.xyz>
2019-01-06 17:54:01 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+use log::{info, warn};
+
 mod currency;
 mod price_in_text;
 
 fn main() {
+    env_logger::init();
+    info!("Starting up");
+    info!("")
 }
