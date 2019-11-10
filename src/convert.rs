15833d0ad1caa01f7a61f8d308d9751c6a3465a5
Clément Joly <oss@171819.xyz>
2019-11-10 18:24:46 +0100

    feat: display all possible price matches with convert

--- src/convert.rs
+++ src/convert.rs
@@ -26,6 +26,7 @@ use std::io::{self, BufRead};
 
 use crate::api;
 use crate::api::RateApi;
+use crate::currency::PriceTag;
 use crate::rate::Rate;
 use crate::MainContext;
 
@@ -66,6 +67,89 @@ fn stdin_buf() -> String {
         .unwrap_or(String::new())
 }
 
+/// Perform and display conversion for a PriceTag
+fn perform_display(ctxt: &MainContext, price_tag: &PriceTag) {
+    let src_currency = price_tag.currency();
+    trace!("src_currency: {}", &src_currency);
+
+    // Get rate
+    trace!("Get db handler");
+    let sh = ctxt.db.store_handle().write().unwrap();
+    trace!("Get rate bucket");
+    let bucket = ctxt.db.bucket_rate(&sh);
+    trace!("Got bucket");
+    let endpoint = api::ExchangeRatesApiIo::new(&ctxt.cfg);
+    trace!("Got API Endpoint");
+    {
+        let rate_from_db = |dst_currency| -> Option<Rate> {
+            debug!("Create read transaction");
+            let txn = sh.read_txn().unwrap();
+            trace!("Get rate from db");
+            let (uptodate_rates, outdated_rates) = ctxt.db.get_rates(
+                &txn,
+                &sh,
+                src_currency,
+                dst_currency,
+                &endpoint.provider_id(),
+            );
+
+            // Remove outdated_rates
+            let mut txnw = sh.write_txn().unwrap();
+            for rate in outdated_rates {
+                ctxt.db.del_rate(&mut txnw, &bucket, rate);
+            }
+
+            let rate = uptodate_rates.last();
+            trace!("rate_from_db: {:?}", rate);
+            rate.map(|r| r.clone())
+        };
+
+        let add_to_db = |rate: Rate| {
+            debug!("Get write transaction");
+            let mut txn = sh.write_txn().unwrap();
+            trace!("Set rate to db");
+            let r = ctxt.db.set_rate(&mut txn, &bucket, rate);
+            trace!("Rate set, result: {:?}", &r);
+            txn.commit().unwrap();
+        };
+
+        let rate_from_api = |dst_currency| -> Option<Rate> {
+            info!("Retrieve rate online");
+            let client = reqwest::Client::new();
+            endpoint.rate(&client, &src_currency, dst_currency)
+        };
+
+        let rates = ctxt.destination_currencies.iter().map(|dst| {
+            rate_from_db(&dst).or_else(|| {
+                let rate = rate_from_api(&dst);
+                if let Some(rate) = &rate {
+                    info!("Set rate to db");
+                    add_to_db(rate.clone());
+                }
+                rate
+            })
+        });
+
+        for rate in rates {
+            if log_enabled!(log::Level::Info) {
+                if let Some(rate) = &rate {
+                    info!("Rate retrieved: {}", &rate);
+                } else {
+                    info!("No rate retrieved");
+                }
+            }
+            trace!("Final rate: {:?}", &rate);
+            if let Some(rate) = rate {
+                // Skip conversion that wouldn’t change currency (like 1 BTC -> 1 BTC)
+                if price_tag.currency() == rate.dst() {
+                    continue;
+                }
+                println!("{} ➜ {}", &price_tag, &price_tag.convert(&rate).unwrap());
+            }
+        }
+    }
+}
+
 /// Parse arguments for convert subcommand and run it
 pub(crate) fn run(ctxt: MainContext, matches: &ArgMatches) {
     let txt;
@@ -77,87 +161,11 @@ pub(crate) fn run(ctxt: MainContext, matches: &ArgMatches) {
     trace!("plain text: {}", &txt);
     let engine: crate::price_in_text::Engine = crate::price_in_text::Engine::new().unwrap();
     let price_tags = engine.all_price_tags(&txt);
-    if let Some(price_tag) = price_tags.get(0) {
-        let src_currency = price_tag.currency();
-        trace!("src_currency: {}", &src_currency);
-
-        // Get rate
-        trace!("Get db handler");
-        let sh = ctxt.db.store_handle().write().unwrap();
-        trace!("Get rate bucket");
-        let bucket = ctxt.db.bucket_rate(&sh);
-        trace!("Got bucket");
-        let endpoint = api::ExchangeRatesApiIo::new(&ctxt.cfg);
-        trace!("Got API Endpoint");
-        {
-            let rate_from_db = |dst_currency| -> Option<Rate> {
-                debug!("Create read transaction");
-                let txn = sh.read_txn().unwrap();
-                trace!("Get rate from db");
-                let (uptodate_rates, outdated_rates) = ctxt.db.get_rates(
-                    &txn,
-                    &sh,
-                    src_currency,
-                    dst_currency,
-                    &endpoint.provider_id(),
-                );
-
-                // Remove outdated_rates
-                let mut txnw = sh.write_txn().unwrap();
-                for rate in outdated_rates {
-                    ctxt.db.del_rate(&mut txnw, &bucket, rate);
-                }
-
-                let rate = uptodate_rates.last();
-                trace!("rate_from_db: {:?}", rate);
-                rate.map(|r| r.clone())
-            };
-
-            let add_to_db = |rate: Rate| {
-                debug!("Get write transaction");
-                let mut txn = sh.write_txn().unwrap();
-                trace!("Set rate to db");
-                let r = ctxt.db.set_rate(&mut txn, &bucket, rate);
-                trace!("Rate set, result: {:?}", &r);
-                txn.commit().unwrap();
-            };
-
-            let rate_from_api = |dst_currency| -> Option<Rate> {
-                info!("Retrieve rate online");
-                let client = reqwest::Client::new();
-                endpoint.rate(&client, &src_currency, dst_currency)
-            };
-
-            let rates = ctxt.destination_currencies.iter().map(|dst| {
-                rate_from_db(&dst).or_else(|| {
-                    let rate = rate_from_api(&dst);
-                    if let Some(rate) = &rate {
-                        info!("Set rate to db");
-                        add_to_db(rate.clone());
-                    }
-                    rate
-                })
-            });
-
-            for rate in rates {
-                if log_enabled!(log::Level::Info) {
-                    if let Some(rate) = &rate {
-                        info!("Rate retrieved: {}", &rate);
-                    } else {
-                        info!("No rate retrieved");
-                    }
-                }
-                trace!("Final rate: {:?}", &rate);
-                if let Some(rate) = rate {
-                    // Skip conversion that wouldn’t change currency (like 1 BTC -> 1 BTC)
-                    if price_tag.currency() == rate.dst() {
-                        continue;
-                    }
-                    println!("{} ➜ {}", &price_tag, &price_tag.convert(&rate).unwrap());
-                }
-            }
-        }
-    } else {
-        println!("No currency found.")
+    if price_tags.len() == 0 {
+        println!("No currency found.");
+        return;
+    }
+    for price_tag in &price_tags {
+        perform_display(&ctxt, price_tag);
     }
 }
